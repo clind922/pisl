676f1a2e4a1b1abce731b130830177e13adec994
Christoffer <lindberg.christoffer@gmail.com>
2021-05-29 11:59:34 +0200

    Added atd script!

---
+++ atd.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Arbitrary text display
+#
+
+import datetime
+import time
+import os
+import math
+import re
+
+from helpers import make_font
+from helpers import time_diff
+from helpers import tdiff_text
+from helpers import is_active_hours
+from helpers import ApiException
+
+from oled_options import get_device
+from luma.core.render import canvas
+from PIL import ImageFont
+from dateutil.parser import parse
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='.env')
+
+screen_data_refresh_delay = 1 # Redraw (cached) data every X seconds
+screen_active_time = 240 # How long the screen is active after button press (during off-hours)
+
+start_time = datetime.datetime.now()
+
+row = 0
+max_rows = 0
+font_size = 15
+width = 0
+height = 0
+line_height = 0
+button_gpio_pin = 15
+
+button_press_time = None
+atd_file_data = None
+
+
+ACTIVE_HOURS = os.getenv("ACTIVE_HOURS")
+
+def button_callback(channel):
+    global button_press_time
+    # Set button press time
+    button_press_time = datetime.datetime.now()
+    print("Button was pushed!")
+
+def button_setup():
+    GPIO.setwarnings(False) # Ignore warning for now
+    GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+    GPIO.setup(button_gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin to be an input pin and set initial value to be pulled low (off)
+    GPIO.add_event_detect(button_gpio_pin,GPIO.RISING,callback=button_callback) # Setup event on pin rising edge
+
+def print_out(left_text='', right_text='', draw=None):
+    global row
+    if draw is None:
+        print('StdOut: ' + left_text + ' ' + right_text)
+    else:
+
+        l_len = len(left_text)
+        r_len = len(right_text)
+        if l_len + r_len >= max_chars:
+            left_text = left_text[:(max_chars - r_len - 1)]
+        else:
+            right_text = ' ' * (max_chars - l_len - r_len - 1) + right_text
+
+        y = row * line_height
+        row += 1
+        draw.text((0, y), left_text + ' ' + right_text, font=font, fill="white")
+
+def draw_atd(draw, data_refresh_delay):
+    global row
+    global last_get_deps
+    f = open("atd.txt", "r")
+    atd_file_data = f.read()
+    atd_file_data = atd_file_data.splitlines()
+    f.close()
+    for line in atd_file_data:
+    	matches = re.findall("(!([a-z_]+)\((\d+)\))", line)
+        for match in matches:
+            ret = {}
+            exec('val = {}({})'.format(match[1], match[2]), {'tdiff_text': tdiff_text}, ret)
+            line = line.replace(match[0], ret['val'])
+    	print_out(line, '', draw=draw)
+
+def main():
+
+    while True:
+        with canvas(device) as draw:
+            # Only draw if started recently
+            if time_diff(start_time) < screen_active_time:
+                draw_atd(draw, data_refresh_delay_normal)
+            # Or only draw if active hours
+            elif ACTIVE_HOURS is not None and is_active_hours(ACTIVE_HOURS, screen_active_time):
+                draw_atd(draw, data_refresh_delay_normal)
+            # Or if button is pressed recently
+            elif button_press_time is not None and time_diff(button_press_time) < screen_active_time:
+                draw_atd(draw, data_refresh_delay_fast)
+        time.sleep(screen_data_refresh_delay)
+
+if __name__ == "__main__":
+    if SL_SITE_ID is None:
+        exit("SL_SITE_ID env missing.")
+    if REALTIME_API_KEY is None:
+        exit("REALTIME_API_KEY env missing.")
+    try:
+        device = get_device()
+        width = device.width
+        height = device.height
+        font = make_font("ProggyTiny.ttf", font_size)
+        # Find char width & height
+        _cw, _ch = (0, 0)
+        for i in range(32, 128):
+            w, h = font.getsize(chr(i))
+            _cw = max(w, _cw)
+            _ch = max(h, _ch)
+        max_chars = width // _cw
+        line_height = _ch
+        max_rows = height // _ch
+        start_time = datetime.datetime.now()
+        button_setup()
+        main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.cleanup() # Clean up
--- helpers.py
+++ helpers.py
@@ -10,6 +10,8 @@ import datetime
 import time
 import croniter
 
+import math
+
 def make_font(name, size):
     font_path = os.path.abspath(os.path.join(
         os.path.dirname(__file__), 'fonts', name))
@@ -24,6 +26,27 @@ def time_diff(dt, absVal=True):
     if absVal:
         diff = abs(diff)
     return int(diff)
+def tdiff(ts, absVal=True):
+    now = time.mktime(time.localtime())
+    diff = ts - now
+    if absVal:
+        diff = abs(diff)
+    return int(diff)
+
+def tdiff_text(ts, absVal=True, max_precision=1):
+    out = []
+    diff = tdiff(ts, absVal)
+    for unit, suffix_plural, suffix in ((3600*24*30*365, 'år', 'år'), (3600*24*30, 'månader', 'månad'), (3600*24, 'dagar', 'dag'), (3600, 'timmar', 'timme'), (60, 'minuter', 'minut'), (1, 'sekunder', 'sekund')):
+        if diff >= unit:
+            val = math.floor(diff / unit)
+            if val != 1:
+                suffix = suffix_plural
+            out.append('{} {}'.format(val, suffix))
+            diff -= val * unit
+            max_precision -= 1
+            if max_precision == 0:
+                break
+    return ', '.join(out)
 
 def is_active_hours(active_hours, limit):
     now = datetime.datetime.now()
